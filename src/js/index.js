fde82bbbb8d9b05eede67e73c07840a09f1e749d
Jonathan Yeo <jonathan@wubble.xyz>
2020-06-16 00:06:20 +1000

    Add Bootstrap

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,14 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const {
+  app,
+  BrowserWindow
+} = require("electron");
+const path = require("path");
 
 // Enable live reload for all the files in the project directory
-require('electron-reload')(__dirname);
+require("electron-reload")(__dirname);
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+if (require("electron-squirrel-startup")) { // eslint-disable-line global-require
   app.quit();
 }
 
@@ -16,8 +19,11 @@ const createWindow = () => {
     height: 600,
   });
 
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  // Define browser window as session
+  // const mainSession = mainWindow.webContents.session
+
+  // Load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, "../html/index.html"));
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools();
@@ -26,24 +32,26 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
+app.on("activate", () => {
+  // On OS X it"s common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+// In this file you can include the rest of your app"s specific main process code. You can also put them in separate files and import them here.
+
+// Print user path to console
+console.log(app.getPath('userData'));
---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+var cmd = process.argv[1];
+
+if (cmd == '--squirrel-firstrun') {
+    // Running for the first time.
+    console.log("This application is running for the first time")
+}
